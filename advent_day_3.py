46ade2c3fb73e080f542bbd9ec81e8d146b8305a
Gerdie <bdme108@gmail.com>
2016-12-21 12:12:30 -0800

    Day 3 Part One complete

--- advent_day_3.py
+++ advent_day_3.py
@@ -1,12 +1,17 @@
 
 good_triangles = 0
+i = 0
 
 with open('input.txt') as input_file:
     for line in input_file:
         line = line.rstrip().split()
         a, b, c = line
-        if a + b > c or a + c > b or b + c > a:
-            continue
-        good_triangles += 1
+        a, b, c = int(a), int(b), int(c)
+        if not (a + b > c and a + c > b and b + c > a):
+            pass
+        else:
+            good_triangles += 1
+        i += 1
 
+print i
 print good_triangles
