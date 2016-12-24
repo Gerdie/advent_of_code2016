114cae448958bed57e076eb51476b1e2b4deb12b
Gerdie <bdme108@gmail.com>
2016-12-24 14:14:06 -0500

    Deciphered repetition code for Part A

---
+++ advent_day_6.py
@@ -0,0 +1,31 @@
+test_input = ["eedadn",
+              "drvtee",
+              "eandsr",
+              "raavrd",
+              "atevrs",
+              "tsrnev",
+              "sdttsa",
+              "rasrtv",
+              "nssdts",
+              "ntnada",
+              "svetve",
+              "tesnvt",
+              "vntsnd",
+              "vrdear",
+              "dvrsen",
+              "enarar"]
+
+result = {}
+result_str = ""
+
+with open('input6.txt') as santa_code:
+    for line in santa_code:
+        line = line.rstrip()
+        for i, char in enumerate(line):
+            result[i] = result.get(i, [])
+            result[i].append(char)
+
+for num in xrange(len(result)):
+    result_str += str(max(result[num], key=result[num].count))
+
+print result_str
