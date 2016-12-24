34908206dc7a8cbd19029c7cba86faafe9d30216
Gerdie <bdme108@gmail.com>
2016-12-24 11:53:57 -0500

    Implemented position indicators in MD5 hash hex

--- advent_day_5.py
+++ advent_day_5.py
@@ -2,7 +2,8 @@ from hashlib import md5
 
 door_code = "reyedfim"
 test_code = "abc"
-result = []
+result = {}
+result_str = ""
 
 i = 0
 
@@ -10,8 +11,19 @@ while len(result) < 8:
     hash_test = md5(door_code + str(i))
     hash_test = hash_test.hexdigest()
     if hash_test[:5] == "00000":
-        result.append(hash_test[5])
+        try:
+            position = int(hash_test[5])
+            character = hash_test[6]
+            if position in result or position > 7:
+                pass
+            else:
+                result[position] = character
+        except ValueError:
+            pass
     i += 1
 
-print result
+for num in xrange(8):
+    result_str += result[num]
+print result_str
 #Part A: f97c354d
+#Part B: 863dde27
