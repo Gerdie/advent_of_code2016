09b9d79886960ea44249a795a8f71e37c59a1f4d
Gerdie <bdme108@gmail.com>
2016-12-30 12:17:26 -0800

    TODO: Reconfigure without saving decompressed file

--- advent_day_9.py
+++ advent_day_9.py
@@ -1,4 +1,4 @@
-word_count = ""
+word_count = 0
 inside = False
 parens = False
 ignore = -1
@@ -7,15 +7,19 @@ test_cases = ["(3x3)XYZ",
               "(6x1)(1x3)A",
               "X(8x2)(3x3)ABCY"]
 
-def compress_file(filename):
-    word_count = ""
-    inside = False
-    parens = False
-    ignore = -1
-    with open(filename) as compressed_file:
-        for line in compressed_file:
+# def compress_file(line):
+# word_count = ""
+# inside = False
+# parens = False
+# ignore = -1
+with open('input9.txt') as compressed_file:
+    for line in compressed_file:
+        while '(' in line:
+            inside = False
+            ignore = -1
+            decomp_line = ""
             for i, char in enumerate(line.rstrip()):
-                if char in "    ":
+                if char in "    " or i < ignore:
                     continue
                 if char == '(':
                     parens = True
@@ -26,19 +30,27 @@ def compress_file(filename):
                     end = i
                     inside = False
                     compress = line[start+1: end].split("x")
+                    print compress
                     # eval_i = i+1
                     char_amt = int(compress[0])
                     repeat_amt = int(compress[1])
-                    word_count += line[i+1: i + char_amt + 1] * repeat_amt
+                    decomp_line += line[i+1: i + char_amt + 1] * repeat_amt
                     ignore = i + char_amt
-                elif i > ignore and not inside:
-                    word_count += char
+                if i > ignore and not inside:
+                    decomp_line += char
+            line = decomp_line
+        word_count += len(line)
 
-    secondary = open('secondary.txt', 'w')
-    secondary.write(word_count)
-    if parens:
-        compress_file('secondary.txt')
-    print len(word_count)
+# secondary = open('secondary.txt', 'w')
+# secondary.write(word_count)
+    # if parens:
+    #     compress_file(word_count)
+    print word_count
 
-compress_file('input9.txt')
+# with open('input9.txt') as compressed_file:
+#     word_total = ""
+#         for line in compressed_file:
+#             word_total += compress_file(line)
+#         if parens:
+#             compress_file(word_count)
 # Part 1: 150914
