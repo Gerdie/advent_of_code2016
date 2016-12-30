4ea57048754c4ac5ecad9d9b4aecd0658fc8b5c1
Gerdie <bdme108@gmail.com>
2016-12-29 18:17:15 -0800

    Recursing while compression markers exist

--- advent_day_9.py
+++ advent_day_9.py
@@ -1,31 +1,44 @@
-word_count = 0
+word_count = ""
 inside = False
-eval_i = None
+parens = False
 ignore = -1
 test_cases = ["(3x3)XYZ",
               "A(2x2)BCD(2x2)EFG",
               "(6x1)(1x3)A",
               "X(8x2)(3x3)ABCY"]
 
-with open('input9.txt') as compressed_file:
-    for line in compressed_file:
-        for i, char in enumerate(line.rstrip()):
-            if char in "    ":
-                continue
-            if char == '(' and i > ignore:
-                start = i
-                inside = True
-            elif char == ')' and i > ignore:
-                end = i
-                inside = False
-                compress = line[start+1: end].split("x")
-                # eval_i = i+1
-                char_amt = int(compress[0])
-                repeat_amt = int(compress[1])
-                word_count += char_amt * repeat_amt
-                ignore = i + char_amt
-            elif i > ignore and not inside:
-                word_count += 1
+def compress_file(filename):
+    word_count = ""
+    inside = False
+    parens = False
+    ignore = -1
+    with open(filename) as compressed_file:
+        for line in compressed_file:
+            for i, char in enumerate(line.rstrip()):
+                if char in "    ":
+                    continue
+                if char == '(':
+                    parens = True
+                    if i > ignore:
+                        start = i
+                        inside = True
+                elif char == ')' and i > ignore:
+                    end = i
+                    inside = False
+                    compress = line[start+1: end].split("x")
+                    # eval_i = i+1
+                    char_amt = int(compress[0])
+                    repeat_amt = int(compress[1])
+                    word_count += line[i+1: i + char_amt + 1] * repeat_amt
+                    ignore = i + char_amt
+                elif i > ignore and not inside:
+                    word_count += char
 
-print word_count
+    secondary = open('secondary.txt', 'w')
+    secondary.write(word_count)
+    if parens:
+        compress_file('secondary.txt')
+    print len(word_count)
+
+compress_file('input9.txt')
 # Part 1: 150914
