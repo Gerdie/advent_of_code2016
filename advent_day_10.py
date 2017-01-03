4171151ab5ff9a592fa6a7322a59c6503a046344
Gerdie <bdme108@gmail.com>
2017-01-03 15:58:00 -0800

    Added Output class and reduce function for part 2

--- advent_day_10.py
+++ advent_day_10.py
@@ -1,12 +1,22 @@
 import re
-# from collections import defaultdict
 
 lines = open('input10.txt').read().split('\n')
 numbers = re.compile(r'(\d+)')
-# all_bots = defaultdict(list)
 
 
-class Bot(object):
+class Output(object):
+
+    def __init__(self, num):
+        self.num = num
+        self.nums = []
+
+    def add_num(self, num2):
+        if num2 not in self.nums:
+            self.nums.append(num2)
+            self.nums.sort()
+
+
+class Bot(Output):
 
     def __init__(self, num):
         self.num = num
@@ -14,10 +24,6 @@ class Bot(object):
         self.high = None
         self.low = None
 
-    def add_num(self, num2):
-        if num2 not in self.nums:
-            self.nums.append(num2)
-            self.nums.sort()
 
 class AllBots(object):
 
@@ -39,6 +45,7 @@ class AllBots(object):
         return repr_string
 
 all_bots = AllBots()
+all_outputs = AllBots()
 
 for line in lines:
     bots = map(int, numbers.findall(line))
@@ -60,28 +67,43 @@ for line in lines:
                 new_low = Bot(bots[1])
                 all_bots.add_bot(new_low)
                 bot.low = new_low
+        else:
+            bot.low = all_outputs.find_bot(bots[1])
+            if not all_outputs.find_bot(bots[1]):
+                new_low = Output(bots[1])
+                all_outputs.add_bot(new_low)
+                bot.low = new_low
         if line[10] == "bot":
             bot.high = all_bots.find_bot(bots[2])
             if not all_bots.find_bot(bots[2]):
                 new_high = Bot(bots[2])
                 all_bots.add_bot(new_high)
                 bot.high = new_high
+        else:
+            bot.high = all_outputs.find_bot(bots[2])
+            if not all_outputs.find_bot(bots[2]):
+                new_high = Output(bots[2])
+                all_outputs.add_bot(new_high)
+                bot.high = new_high
 
 print "All bots added: ", all_bots
+print "All outputs added: ", all_outputs
 
 skip = set()
-searching = True
-while searching:
+while len(skip) < len(all_bots.bots):
     for bot in all_bots.bots:
         if bot in skip:
             continue
         if len(bot.nums) > 1:
             if bot.nums == [17, 61]:
                 print "Part 1: ", bot.num
-                searching = False
-            if bot.low:
-                bot.low.add_num(bot.nums[0])
-            if bot.high:
-                bot.high.add_num(bot.nums[-1])
+            bot.low.add_num(bot.nums[0])
+            bot.high.add_num(bot.nums[-1])
             skip.add(bot)
             print "Completed: bot", bot.num
+
+part_two = reduce((lambda x, y: x*y), [output.nums[0] for output in all_outputs.bots if output.num in [0, 1, 2]])
+print "Part 2: ", part_two
+
+# Part 1: 141
+# Part 2: 1209
